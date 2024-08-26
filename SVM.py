1bf1b0c7b918646dd8580ace17698fb70362978e
Fhurky <furkocal@gmail.com>
2024-08-26 15:42:06 +0300

    Adding Libraries

--- SVM.py
+++ SVM.py
@@ -6,6 +6,10 @@ Created on Mon Aug 26 15:36:05 2024
 """
 
 import pandas as pd
+import sklearn.svm 
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import log_loss, confusion_matrix, jaccard_score
 
 data = pd.read_csv("Cancer_Data.csv")
 data = data.drop(["id"], axis = 1)
