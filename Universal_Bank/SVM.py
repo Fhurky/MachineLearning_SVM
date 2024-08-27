6aee5fa7bc2d760ad69841ef65f50cf897bb386e
Fhurky <furkocal@gmail.com>
2024-08-27 23:16:38 +0300

    Updating

--- Universal_Bank/SVM.py
+++ Universal_Bank/SVM.py
@@ -10,3 +10,9 @@ import pandas as pd
 data = pd.read_csv("UniversalBank.csv")
 
 data = data.drop(["ID"], axis = 1)
+
+Y_personal_loan = data.iloc[:,8:9]
+Y_Securities_Account = data.iloc[:,9:10]
+Y_CD_account = data.iloc[:,10:11]
+Y_Online = data.iloc[:,11:12]
+Y_Credit_card = data.iloc[:,12:13]
